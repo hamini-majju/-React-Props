a91b8ed000de151beade7df5f6100e78879cdd45
unknown <csclab1sys121@csclab1sys121.parjanya.pbsiddhartha.ac.in>
2022-12-01 16:51:41 +0530

    states&events

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 //import ClassComponent from './ClassComponent'
-import PropsExample from './PropsExample'
+//import PropsExample from './PropsExample'
+import FuncProps from './FuncProps'
+import StateExample from './StateExample'
+import DemoExample from './DemoExample'
+import Events from './Events'
 
 function App(){
 
   return(
-        <section>
-          <PropsExample name="hamini" age="19"/>
+          <section>
+          {/*<PropsExample name="hamini" age="19"/>
           <PropsExample name="chinni" age="19"/>
-          {/*<h1>how are u</h1>
+          <h1>how are u</h1>
           <p>whats going on here</p>
           <img src="https://wallpaperaccess.com/full/1204217.jpg" width="30%" alt="pic1"/>
-          <ClassComponent/>*/}
-        </section>
-
+          <ClassComponent*/}  
+          <FuncProps carname="Range Rover" year="1996"/>
+          <StateExample/>
+          <DemoExample price="1st price"/>    
+          <Events/>
+          </section>
+    
       )
   }
   export default App;
