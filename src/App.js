59796f6cdede79eeefb70aabaf4a1efe4a86ff84
unknown <csclab1sys121@csclab1sys121.parjanya.pbsiddhartha.ac.in>
2022-12-02 16:50:16 +0530

    map&state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 //import ClassComponent from './ClassComponent'
 //import PropsExample from './PropsExample'
-import FuncProps from './FuncProps'
+//import FuncProps from './FuncProps'
 import StateExample from './StateExample'
-import DemoExample from './DemoExample'
-import Events from './Events'
+//import DemoExample from './DemoExample'
+//import Events from './Events'
+//import JSX from './JSX'
 
 function App(){
 
@@ -15,10 +16,11 @@ function App(){
           <p>whats going on here</p>
           <img src="https://wallpaperaccess.com/full/1204217.jpg" width="30%" alt="pic1"/>
           <ClassComponent*/}  
-          <FuncProps carname="Range Rover" year="1996"/>
-          <StateExample/>
-          <DemoExample price="1st price"/>    
-          <Events/>
+         {/* <FuncProps carname="Range Rover" year="1996"/>
+            <DemoExample price="1st price"/>  
+            <Events/>
+            <JSX/>*/}
+         <StateExample/>
           </section>
     
       )
--- src/Events.js
+++ src/Events.js
@@ -2,17 +2,32 @@ import {Component} from 'react'
 
 class Events extends Component{
 	state={
-		username:""
+		username:"",
+		number:""
 	}
-	handleChange=()=>{
-		alert()
+	handleClick=()=>{
+		console.log(this.state.username)
+		console.log(this.state.number)
+	}
+	handleChange=(e)=>{
+		this.setState({
+			[e.target.name]:e.target.value
+		})
 	}
 	render(){
 		return(
-			<div>
-			<input onChange={this.handleChange} placeholder="Enter username"/>
+		<div>
+			<input
+			 name="username"
+			 onChange={this.handleChange}
+			 placeholder="Enter username"/><br/>
+			 <input
+			  name="number"
+			  onChange={this.handleChange}
+			  placeholder="Enter number"/>
+			 <button onClick={this.handleClick}>Submit</button>
 			</div>
-			)
+		)
 	}
 }
 export default Events
